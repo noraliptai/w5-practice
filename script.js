be3731c4e7ee69da2e9b65888abb0342fde54d90
Your Name <you@example.com>
2023-10-26 10:47:30 +0200

    fetch in a separate function

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 console.log('loaded')
 
 const rootElement = document.querySelector("#root")
-console.log(rootElement)
+console.dir(rootElement)
 
-rootElement.innerHTML = "sziasztok"
+const fetchUrl = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
 
 const countryComponent = (country) => `
-    <h2>country name: ${country.name.common}</h2>
-    <h3>country pop: ${country.population}</h3>
+    <div class="country">
+        <h2>country name: ${country.name.common}</h2>
+        <h3>country pop: ${country.population}</h3>
+    </div>
 `
 
-async function init() {
-    const response = await fetch("https://restcountries.com/v3.1/all")
-    const data = await response.json()
-    //console.log(data)
-    
-    data.forEach(country => rootElement.insertAdjacentHTML("afterbegin", countryComponent(country)))
+async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
+    const data = await fetchUrl("https://restcountries.com/v3.1/all")
 
+    data.forEach(country => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)))
 }
 
 init()
