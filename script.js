6a1075b6837fffb10fc88c45f02b420e5a5e1fe2
Your Name <you@example.com>
2023-10-27 18:03:56 +0200

    header, swapi prepared

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 console.log('loaded')
 
 const rootElement = document.querySelector("#root")
-console.dir(rootElement)
 
 const fetchUrl = async (url) => {
     const response = await fetch(url)
     const data = await response.json()
     return data
 }
+
+const skeletonComponent = () => `
+    <header></header>
+    <main></main>
+    <footer></footer>
+`
+
+const headerButtonComponent = (buttonText) => `
+    <button id="${buttonText.toLowerCase()}">${buttonText}</button>
+`
 
 const countryComponent = (country) => `
     <div class="country">
@@ -16,17 +25,29 @@ const countryComponent = (country) => `
     </div>
 `
 
+const makeDomFromData = (data, rootElement, componentFn) => {
+    data.forEach(value => rootElement.insertAdjacentHTML("beforeend", componentFn(value)))
+}
+
 async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
+    rootElement.insertAdjacentHTML("beforeend", skeletonComponent())
+    const headerElement = document.querySelector("header")
+    const mainElement = document.querySelector("main")
+    const footerElement = document.querySelector("footer")
+
+    makeDomFromData(["Countries", "Swapi", "Contact"], headerElement, headerButtonComponent)
+    const swapiButtonElement = document.querySelector("#swapi")
+    swapiButtonElement.addEventListener("click", () => {
+        mainElement.innerHTML = "Under construction!!!!!!!!!!!!"
+    })
+
     const data = await fetchUrl("https://restcountries.com/v3.1/all")
-
-    data.forEach(country => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)))
-
-/*     const countryElement = document.querySelector("div.country")
-    countryElement.addEventListener("click", () => countryElement.classList.toggle("clicked")) */
-
+    makeDomFromData(data, mainElement, countryComponent)
     const countryElements = document.querySelectorAll("div.country")
     countryElements.forEach((countryElement) => {
-        countryElement.addEventListener("click", () => countryElement.classList.toggle("clicked"))
+        countryElement.addEventListener("click", () => {
+            countryElement.classList.toggle("clicked")
+        })
     })
 }
 
